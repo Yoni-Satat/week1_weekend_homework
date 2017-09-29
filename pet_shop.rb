4efbb8677f74671e394a2705806cf57109549878
Yoni Satat <yoni.satat@gmail.com>
2017-09-29 15:33:27 +0100

    test 10 passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -31,3 +31,11 @@ def pets_by_breed(pet_shop, breed)
   end
   return pets_found
 end
+
+def find_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    if (name == pet[:name])
+      return pet
+    end
+  end
+end
